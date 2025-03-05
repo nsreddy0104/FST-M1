1540420fd7c43ef8be35e5ed753931c24efc93d4
nsreddy0104 <165196612+nsreddy0104@users.noreply.github.com>
2025-03-05 11:25:19 +0530

    Add files via upload

---
+++ Java/Activities/Activity1.java
@@ -0,0 +1,13 @@
+package activities;
+
+public class Activity1 {
+
+	public static void main(String[] args) {
+		
+		Car car= new Car("Red","Manual",2020);
+		
+		car.displayCharacteristics();
+		car.accelarate();
+		car.brake();
+	}
+}
